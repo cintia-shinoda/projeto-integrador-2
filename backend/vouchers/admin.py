6e2ce9f38ff3e1e45de4636c0a804d20b032a15e
Cintia Izumi Shinoda <shinoda.c.i@gmail.com>
2024-10-03 18:55:39 -0300

    update: models.py + add: superuser(admin.py)

--- backend/vouchers/admin.py
+++ backend/vouchers/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Vouchers
 
 # Register your models here.
+class VouchersAdmin(admin.ModelAdmin):
+    list_display = ('nome', 'tel', 'voucher', 'dataCadastro')
+
+admin.site.register(Vouchers, VouchersAdmin)
--- backend/vouchers/models.py
+++ backend/vouchers/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from datetime import datetime
 
-# Create your models here.
+
+# informações (campos) que serão solicitados ao usuário no cadastro
+class Vouchers(models.Model):
+    nome = models.CharField(max_length=100)
+    tel = models.CharField(max_length=15)
+    voucher = models.CharField(max_length=10, null=True)
+    dataCadastro = models.DateTimeField(default=datetime.now)
+
+    def __str__(self):
+        return self.nome
